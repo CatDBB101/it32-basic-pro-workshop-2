f8a303f247cdf734d62af09a081c6a395378c447
CatDBB <catdbb1000@gmail.com>
2026-07-14 20:01:26 +0700

    feat: position for new employee

--- 2b.py
+++ 2b.py
@@ -1,14 +1,14 @@
-name = input("name :")
-age = int(input("age :"))
-height = int(input("height(cm) :"))
-power = int(input("power(1-10) :"))
-money = int(input("money :"))
+name = input("name : ")
+age = int(input("age : "))
+height = int(input("height(cm) : "))
+power = int(input("power(1-10) : "))
+money = int(input("money : "))
 
 # Pass or not dicision
 
 isPass = True
 print("=" * 50)
-if age < 18:
+if age >= 18:
     print("\tAge must more than or equal 18.")
     isPass = False
 if power < 2:
@@ -22,3 +22,14 @@ else:
     exit()
 
 # Position dicision
+
+if age >= 20 and power < 5:
+    print("Your position is \"Gunner\".")
+elif power >= 5 and power <= 8:
+    print("Your position is \"Guard\".")
+elif power >= 9:
+    print("Your position is \"Giant Guard\".")
+elif money >= 10_000_000:
+    print("Your position is \"Boss\".")
+else:
+    print("Your positon is \"Janitor\".")
